7f2abc141c0004c6f5d3f3edb4f2a25f7eaba7f0
MakkaAkhrieva <makka.akhrieva00@mail.ru>
2023-03-21 14:44:14 +0300

    enums

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 "use strict";
-const skill = [1, "Dev"];
-const skills = ["dev", "deadfad"];
+var StatusCode;
+(function (StatusCode) {
+    StatusCode[StatusCode["SUCCESS"] = 0] = "SUCCESS";
+    StatusCode[StatusCode["IN_PROGRESS"] = 1] = "IN_PROGRESS";
+    StatusCode[StatusCode["FAILED"] = 2] = "FAILED";
+})(StatusCode || (StatusCode = {}));
+const res = {
+    message: "Payment  success",
+    statusCode: StatusCode.SUCCESS,
+};
+function compute() {
+    return 3;
+}
+const res2 = 1 /* Roles.ADMIN */;
--- app.ts
+++ app.ts
@@ -1,3 +1,21 @@
-const skill: readonly [number, string] = [1, "Dev"];
+enum StatusCode {
+  SUCCESS,
+  IN_PROGRESS,
+  FAILED,
+}
 
-const skills: ReadonlyArray<string> = ["dev", "deadfad"];
+const res = {
+  message: "Payment  success",
+  statusCode: StatusCode.SUCCESS,
+};
+
+function compute() {
+  return 3;
+}
+
+const enum Roles {
+  ADMIN = 1,
+  USER = 2,
+}
+
+const res2 = Roles.ADMIN;
