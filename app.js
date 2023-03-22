99169da02e512b602a0680aacf4c1720ec2ca409
MakkaAkhrieva <makka.akhrieva00@mail.ru>
2023-03-22 11:22:28 +0300

    type aliases

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 "use strict";
-var StatusCode;
-(function (StatusCode) {
-    StatusCode[StatusCode["SUCCESS"] = 0] = "SUCCESS";
-    StatusCode[StatusCode["IN_PROGRESS"] = 1] = "IN_PROGRESS";
-    StatusCode[StatusCode["FAILED"] = 2] = "FAILED";
-})(StatusCode || (StatusCode = {}));
-const res = {
-    message: "Payment  success",
-    statusCode: StatusCode.SUCCESS,
-};
-function compute() {
-    return 3;
+/* type httpMethod = "post" | "get";
+
+type coolString = string;
+
+function fetchWithAuth(url: string, method: httpMethod): 1 | -1 {
+  return 1;
 }
-const res2 = 1 /* Roles.ADMIN */;
+
+fetchWithAuth("d", "post");
+
+let methods = "post";
+fetchWithAuth("dsd", methods as "post"); */
+let userWithRole = {
+    user: {
+        name: "Вася",
+        age: 25,
+        skills: [],
+    },
+    role: {
+        name: "admin",
+        id: 1,
+    },
+};
--- app.ts
+++ app.ts
@@ -1,8 +1,37 @@
-function fetchWithAuth(url: string, method: "post" | "get"): 1 | -1 {
+/* type httpMethod = "post" | "get";
+
+type coolString = string;
+
+function fetchWithAuth(url: string, method: httpMethod): 1 | -1 {
   return 1;
 }
 
 fetchWithAuth("d", "post");
 
 let methods = "post";
-fetchWithAuth("dsd", methods as "post");
+fetchWithAuth("dsd", methods as "post"); */
+
+type User = {
+  name: string;
+  age: number;
+  skills: string[];
+};
+
+type Role = {
+  name: string;
+  id: number;
+};
+
+type UserWithRole = { user: User; role: Role };
+
+let userWithRole: UserWithRole = {
+  user: {
+    name: "Вася",
+    age: 25,
+    skills: [],
+  },
+  role: {
+    name: "admin",
+    id: 1,
+  },
+};
